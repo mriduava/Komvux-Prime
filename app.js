a0eb3bd57bc7461370ba23e1e651054c5098fcaf
MRIDUAVA <34859254+mriduava@users.noreply.github.com>
2019-03-19 06:51:16 +0100

    code refactored

--- app.js
+++ app.js
@@ -18,141 +18,37 @@ $(() => {
         { name: 'ALISA', price: 1120, id: 15, description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde fugit, vel quibusdam fugiat at atque vero, minima ratione tempora architecto sit aperiam similique in quidem, iste nemo debitis quaerat nisi!', picture: 'img/15.jpg', category: 'Winter Mys' }
     ];
 
-  
-
-    //-------------------to sort by price with only one button -----------//
-    //    let counter = 0;
-    //     // to connect sort button with js
-    //     let sortButton = document.querySelector(".sortbutton");
-    //     // to create click funtion, set button original value 0, after click becomes 1 
-
-    //     sortButton.addEventListener("click",function(){
-    //     if (counter === 0){
-    //         counter = 1;
-    //     let sortArray = products.sort(function(a,b){
-    //         if(a.price >b.price){
-    //             return 1;
-    //         }
-    //         else {return -1};
-
-    //     })
-
-    // appendList(sortArray);
-    // }
-    // else if (counter === 1){
-    //     console.log(counter);
-    //     counter = 0;
-    // let sortArray = products.sort(function(a,b){
-    //     if(a.price >b.price){
-    //         return -1;
-    //     }
-    //     else {return 1};
-
-    // })
-    // appendList(sortArray);
-    // }
-
-    // }) 
-
-
-    // --------------------to sort price from low to high or high to low from top navigation -----------------//
-
-
-
-    let sortPriceFromLow = document.querySelector("#sort-price-low-high");
-    sortPriceFromLow.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        let sortArray = products.sort((a, b) => a.price > b.price ? 1 : -1);
-        appendList(sortArray);
-    });
-
-    let sortPriceFromHigh = document.querySelector("#sort-price-high-low");
-    sortPriceFromHigh.addEventListener("click", (e) => {
-        e.preventDefault();
-        let sortArray = products.sort((a, b) => a.price < b.price ? 1 : -1);
-        appendList(sortArray);
-    });
-
-    // ------------------- search product ---------------------------//
-    /* although it can search out the things but seached out product  can not be added into cart.. */
-
-    let searchField = document.querySelector(".searchfield");
-    searchField.addEventListener("keyup", function (e) {
-        // console.log (e.target.value);
-        let myTarget = e.target.value;
-        let filterList = products.filter(
-            (product) => {
-                return product.name.toLowerCase().indexOf(myTarget.toLowerCase()) !== -1
-
-            }
-        )
-        appendList(filterList);
-    })
-
+ 
     // ----------------- to show all products on the page -------------------//
-
     const appendList = (array) => {
         const template = array.map((item, id) => {
-            return `
-            
-            
-            <div class="card product d-inline-flex mt-5 mb-2 mr-3 ml-3" style="width: 15rem;" id="${item.id}">
+            return `<div class="card product d-inline-flex mt-5 mb-2 mr-3 ml-3" style="width: 15rem;" id="${item.id}">
             <img src="${item.picture}" class="card-img-top" alt="${item.description}">
             <div class="card-body mr-auto ml-auto">
             <h5 class="card-title">${item.name} - <span class="category">${item.category}</span> <small>${item.price}kr</small></h5>
             <p class="card-text">${item.description}</p>
             <button type="button" id="${item.id}">Buy Now!</button>
-            </div>
-
-                    `;
+            </div>`;
         });
         $('.product-list').html(template);
     }
     appendList(products);
 
-
-   //--------------------Cart & add to cart ---------------------// 
+    //--------------------Cart & add to cart ---------------------// 
     let cart = [];
-
     const addToCart = (array, id) => {
-        let a = array.find(i => i.id === id); //after clicking the buying button, if the button's id equals item's id, push the item a into the cart
-
+        let a = array.find(i => i.id === id);
         cart.push(a);
-
-
-        const item = `
-                <li class="item pl-3 pb-1" id="${a.id}">
+        const item = `<li class="item pl-3 pb-1" id="${a.id}">
                 <div class="d-flex">
                 <span class="pb-1 cartitem"><i class="fas fa-heart"></i> ${a.name}</span> <p class="ml-5 pl-5"> ${a.price} kr</p>
                 <button type="button" class="btn text-dark d-flex mt-0 pt-0 mr-0 pr-0"><i class="fas fa-times"></i></button>
                 </div>
-                </li>
-                `;
-
-        $('span.amount').text(cart.length);
+                </li>`;
         $('.cart-list').append(item);
-
+        $('span.amount').text(cart.length);
     }
 
-
-//----------------remove from cart one by one ------------------------//
-    const removeFromCart = (array, removedItem) => {
-        array.splice(removedItem, 1);
-
-    }
-
-    const populateCart = (array) => {
-        let item = `
-             
-                <li class"item" id="${array.id}">
-                <span class="cartitem">${array.name}</span>
-                <button type="button" class="cancelbtn"><i class="fas fa-times"></i></button>
-                </li>
-                `;
-
-        $('span.amount').text(array.length);
-    }
 
     //-------------------- To calculateTotal Price -----------------//
     const totalAmount = () => {
@@ -162,54 +58,103 @@ $(() => {
 
         })
         return total;
+    }   
+    
+    const buyClick = ()=>{
+        $('.product').on('click', 'button', (e) => {
+            let id = e.currentTarget.id;
+            addToCart(products, +id);
+            $(".totalAmount").text(totalAmount);
+        });        
     }
+    buyClick();
 
-    //I wonder what this part is about ? what is (.product)? did I link to wrong place?
-
-    $('.product').on('click', 'button', (e) => {
-        let id = e.currentTarget.id;
-        // +id : + sign makes it a number
-        addToCart(products, +id); // call the addToCart Function with arrguments products array and button id( which is set quals to product id)
-
-        $(".totalAmount").text(totalAmount);
-
+    // --------------------to sort price from low to high or high to low from top navigation -----------------//
+    let sortPriceFromLow = document.querySelector("#sort-price-low-high");
+    sortPriceFromLow.addEventListener("click", () => {
+        let sortArray = products.sort((a, b) => a.price > b.price ? 1 : -1);
+        appendList(sortArray);
+        // $('.product').on('click', 'button', (e) => {
+        //     let id = e.currentTarget.id;
+        //    addToCart(products, +id);
+        //     $(".totalAmount").text(totalAmount);
+        // });
+        buyClick();
     });
 
+    let sortPriceFromHigh = document.querySelector("#sort-price-high-low");
+    sortPriceFromHigh.addEventListener("click", () => {
+        let sortArray = products.sort((a, b) => a.price < b.price ? 1 : -1);
+        appendList(sortArray);
+        // $('.product').on('click', 'button', (e) => {
+        //     let id = e.currentTarget.id;
+        //     addToCart(products, +id);
+        //     $(".totalAmount").text(totalAmount);
+        // });
+        buyClick();
+    });
+
+    // ------------------- search product ---------------------------//
+    /* although it can search out the things but seached out product  can not be added into cart.. */
+
+    let searchField = document.querySelector(".searchfield");
+    searchField.addEventListener("keyup", function (e) {
+        // console.log (e.target.value);
+        let myTarget = e.target.value;
+        let filterList = products.filter((product) => {
+                return product.name.toLowerCase().indexOf(myTarget.toLowerCase()) !== -1;
+            }
+        )
+        appendList(filterList);
+ 
+        // $('.product').on('click', 'button', (e) => {
+        //     let id = e.currentTarget.id;
+        //     addToCart(products, +id); 
+        //     $(".totalAmount").text(totalAmount);
+        // });
+        buyClick();
+    })
+
+
+    //----------------remove from cart one by one ------------------------//
+    const removeFromCart = (array, removedItem) => {
+        array.splice(removedItem, 1);
+    }
+
+    // const populateCart = (array) => {
+    //     let item = `<li class"item" id="${array.id}">
+    //             <span class="cartitem">${array.name}</span>
+    //             <button type="button" class="cancelbtn"><i class="fas fa-times"></i></button>
+    //             </li>`;
+    //     $('span.amount').text(array.length);
+    // }
+
+    // Item removed function
     $('.cart-list').on('click', 'button', (e) => {
         let item = $(e.currentTarget).closest('li').remove();
         removeFromCart(cart, item);
-        populateCart(cart);
-        
-
+        // populateCart(cart);
         $(".totalAmount").text(totalAmount);
-
+        $('span.amount').text(cart.length);
     });
 
 
-  //---------------------------- Clear All -------------------------//
-
-  /* It sucessfully clear all the items and amount in the cart but 
-  when I re-select the products, item will not show in the cart, only total amount shows up */
-  
+    //---------------------------- Clear All -------------------------//
     const removeAll = () => {
-        $('.cart-list').remove();
-        cart =[];
+        $('.cart-list').text('');
+        cart = [];
+    };
 
-        }; 
-   
-    $('.clear-all').click((e) => {
+    $('.clear-all').on('click', (e) => {
         removeAll();
-        $('span.amount').text(0);
-        $(".totalAmount").text(totalAmount);
-
-    });
-
-  
-
-    // document.getElementById('dropdownitem').addEventListener('click', function (event) {
-    //     event.stopPropagation();
-    //   });
-
-
-
+        $('span.amount').text(cart.length);
+        $(".totalAmount").text(totalAmount); 
+     
+        // $('.product').on('click', 'button', (e) => {
+        //     let id = e.currentTarget.id;            
+        //     addToCart(products, +id);
+        //     $(".totalAmount").text(totalAmount);
+        // });
+    }); 
+     
 }); 
